1e03c8fd7007f3ffa57b99ab5451db7f2644dd31
applzz <hws0602@naver.com>
2024-03-07 07:51:39 +0900

    240307_2

--- servlet/src/main/java/hello/servlet/web/servlet/MemberFormServlet.java
+++ servlet/src/main/java/hello/servlet/web/servlet/MemberFormServlet.java
@@ -8,13 +8,34 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 @WebServlet(name = "memberFormServlet", urlPatterns = "/servlet/members/new-form")
 public class MemberFormServlet extends HttpServlet {
 
     private MemberRepository memberRepository = MemberRepository.getInstance();
     @Override
-    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        response.setContentType("text/html");
+        response.setCharacterEncoding("utf-8");
+
+        // response.getWriter(); 로 write 를 얻어올 수 있다.
+        PrintWriter w = response.getWriter();
+        w.write("<!DOCTYPE html>\n" +
+                "<html>\n" +
+                "<head>\n" +
+                "    <meta charset=\"UTF-8\">\n" +
+                "    <title>Title</title>\n" +
+                "</head>\n" +
+                "<body>\n" +
+                "<form action=\"/servlet/members/save\" method=\"post\">\n" +
+                "    username: <input type=\"text\" name=\"username\" />\n" +
+                "    age:      <input type=\"text\" name=\"age\" />\n" +
+                " <button type=\"submit\">전송</button>\n" + "</form>\n" +
+                "</body>\n" +
+                "</html>\n");
+
 
     }
 }
