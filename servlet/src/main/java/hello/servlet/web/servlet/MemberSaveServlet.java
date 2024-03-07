bf7deafd884e3a8b508834df2e5a6d6ff46063d8
Wokwok2 <hws0602@naver.com>
2024-03-07 12:32:22 +0900

    240307_3

---
+++ servlet/src/main/java/hello/servlet/web/servlet/MemberSaveServlet.java
@@ -0,0 +1,22 @@
+package hello.servlet.web.servlet;
+
+
+import hello.servlet.domain.member.MemberRepository;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(name = "memberSaveServlet", urlPatterns = "/servlet/members/save")
+public class MemberSaveServlet extends HttpServlet {
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String usernama = request.getParameter("usernama");
+        int age = Integer.parseInt(request.getParameter("age"));
+    }
+}
